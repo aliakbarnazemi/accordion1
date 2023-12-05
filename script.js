fdbc14ae657ec0885b77440d2448191baaa8f352
Aliakbar Nazemi <139851676+aliakbarnazemi@users.noreply.github.com>
2023-12-05 09:42:52 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const accordionItems = document.querySelectorAll('.accordion-item');
+const accordionItems = document.querySelectorAll('.accordion-item');
 
-    accordionItems.forEach(item => {
-        const header = item.querySelector('.accordion-header');
+accordionItems.forEach(item => {
+    const header = item.querySelector('.accordion-header');
 
-        header.addEventListener('click', function () {
-            // Toggle the visibility of the content when the header is clicked
-            const content = item.querySelector('.accordion-content');
-            content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'block' : 'none';
-        });
+    header.addEventListener('click', () => {
+        const content = item.querySelector('.accordion-content');
+        content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'block' : 'none';
     });
 });
